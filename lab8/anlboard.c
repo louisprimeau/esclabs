fdb9d6f869f7945e52c8e8563a5f584512436c11
Louis Primeau <louis@Louiss-MacBook-Pro.local>
2018-10-31 01:13:39 -0400

    Testing Atom Commit

--- lab8/anlboard.c
+++ lab8/anlboard.c
@@ -3,29 +3,61 @@ int anlboard(int *T, int sizeT){
   if((sizeT > 9) || (sizeT < 1)){
     returnval = 0;
   }
-  else if((T[0] == T[1]) && (T[1] == T[2])){
-    returnval = 1;
+  else if((T[0] == T[1]) && (T[1] == T[2]) && (T[0] != 0)){
+    if(T[0] == 'X'){
+      returnval = 1;
+    }else if(T[0] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[3] == T[4]) && (T[4] == T[5])){
-    returnval = 1;
+  else if((T[3] == T[4]) && (T[4] == T[5]) && (T[3] != 0)){
+    if(T[3] == 'X'){
+      returnval = 1;
+    }else if(T[3] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[6] == T[7]) && (T[7] == T[8])){
-    returnval = 1;
+  else if((T[6] == T[7]) && (T[7] == T[8]) && (T[6] != 0)){
+    if(T[6] == 'X'){
+      returnval = 1;
+    }else if(T[6] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[0] == T[3]) && (T[3] == T[6])){
-    returnval = 1;
+  else if((T[0] == T[3]) && (T[3] == T[6]) && (T[0] != 0)){
+    if(T[0] == 'X'){
+      returnval = 1;
+    }else if(T[0] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[1] == T[4]) && (T[4] == T[7])){
-    returnval = 1;
+  else if((T[1] == T[4]) && (T[4] == T[7]) && (T[1] != 0)){
+    if(T[1] == 'X'){
+      returnval = 1;
+    }else if(T[1] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[2] == T[5]) && (T[5] == T[8])){
-    returnval = 1;
+  else if((T[2] == T[5]) && (T[5] == T[8]) && (T[2] != 0)){
+    if(T[2] == 'X'){
+      returnval = 1;
+    }else if(T[2] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[0] == T[4]) && (T[4] == T[8])){
-    returnval = 1;
+  else if((T[0] == T[4]) && (T[4] == T[8]) && (T[0] != 0)){
+    if(T[0] == 'X'){
+      returnval = 1;
+    }else if(T[0] == 'O'){
+      returnval = 2;
+    }
   }
-  else if((T[2] == T[4]) && (T[4] == T[6])){
-    returnval = 1;
+  else if((T[2] == T[4]) && (T[4] == T[6]) && (T[2] != 0)){
+    if(T[2] == 'X'){
+      returnval = 1;
+    }else if(T[2] == 'O'){
+      returnval = 2;
+    }
   }
   return(returnval) 
 }
