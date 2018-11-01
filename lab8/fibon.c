9916572b3fb4e76c95da0e0e19908e47cba8c322
Louis Primeau <louis@Louiss-MacBook-Pro.local>
2018-11-01 15:37:00 -0400

    fibon.c

---
+++ lab8/fibon.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fibo(int n);
+
+int main(void) {
+   int i = 0;
+   int arg = 0;
+   printf("Enter a number:\n");
+   scanf("%d",&arg);
+   /*printf("\n");*/
+   for (i=0;i<arg;i=i+1){
+      int f = fibo(i);
+      printf("fibo(%d)=%d\n",i,f);
+   }
+
+   return 0;
+}
+
+int fibo(int n){
+   if((n == 0) || (n == 1)){
+     return(1);
+   }else{
+   return(fibo(n - 1) + fibo(n - 2));}
+}
