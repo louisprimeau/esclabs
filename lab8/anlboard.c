ebbfc80560ce9c3f42e5e391f0356f53332181b3
Louis Primeau <louis@Louiss-MacBook-Pro.local>
2018-11-01 14:13:00 -0400

    anlboard.c

--- lab8/anlboard.c
+++ lab8/anlboard.c
@@ -1,63 +1,34 @@
 int anlboard(int *T, int sizeT){
+  if(sizeT > 9 || sizeT < 1){
+    return(-1);
+  }
   int returnval = 0;
-  if((sizeT > 9) || (sizeT < 1)){
-    returnval = 0;
+  if(T[0] == T[1] == T[2] != 0){
+    returnval = T[0];
+  }else if(T[3] == T[4] == T[5] != 0){
+    returnval = T[3];
+  }else if(T[6] == T[7] == T[8] != 0){
+    returnval = T[6];
+  }else if(T[0] == T[3] == T[6] != 0){
+    returnval = T[0];
+  }else if(T[1] == T[4] == T[7] != 0){
+    returnval = T[1];
+  }else if(T[2] == T[5] == T[8] != 0){
+    returnval = T[2];
+  }else if(T[0] == T[4] == T[8] != 0){
+    returnval = T[0];
+  }else if(T[2] == T[4] == T[6] != 0){
+    returnval = T[2];
   }
-  else if((T[0] == T[1]) && (T[1] == T[2]) && (T[0] != 0)){
-    if(T[0] == 'X'){
-      returnval = 1;
-    }else if(T[0] == 'O'){
-      returnval = 2;
+  int openmoves = 0;
+  int i = 0;
+  for(i = 0; i < 9; i++){
+    if(T[i] == 0){
+      openmoves++;
     }
   }
-  else if((T[3] == T[4]) && (T[4] == T[5]) && (T[3] != 0)){
-    if(T[3] == 'X'){
-      returnval = 1;
-    }else if(T[3] == 'O'){
-      returnval = 2;
-    }
+  if(openmoves == 0 && returnval == 0){
+    returnval = 3;
   }
-  else if((T[6] == T[7]) && (T[7] == T[8]) && (T[6] != 0)){
-    if(T[6] == 'X'){
-      returnval = 1;
-    }else if(T[6] == 'O'){
-      returnval = 2;
-    }
-  }
-  else if((T[0] == T[3]) && (T[3] == T[6]) && (T[0] != 0)){
-    if(T[0] == 'X'){
-      returnval = 1;
-    }else if(T[0] == 'O'){
-      returnval = 2;
-    }
-  }
-  else if((T[1] == T[4]) && (T[4] == T[7]) && (T[1] != 0)){
-    if(T[1] == 'X'){
-      returnval = 1;
-    }else if(T[1] == 'O'){
-      returnval = 2;
-    }
-  }
-  else if((T[2] == T[5]) && (T[5] == T[8]) && (T[2] != 0)){
-    if(T[2] == 'X'){
-      returnval = 1;
-    }else if(T[2] == 'O'){
-      returnval = 2;
-    }
-  }
-  else if((T[0] == T[4]) && (T[4] == T[8]) && (T[0] != 0)){
-    if(T[0] == 'X'){
-      returnval = 1;
-    }else if(T[0] == 'O'){
-      returnval = 2;
-    }
-  }
-  else if((T[2] == T[4]) && (T[4] == T[6]) && (T[2] != 0)){
-    if(T[2] == 'X'){
-      returnval = 1;
-    }else if(T[2] == 'O'){
-      returnval = 2;
-    }
-  }
-  return(returnval) 
+  return(returnval);
 }
