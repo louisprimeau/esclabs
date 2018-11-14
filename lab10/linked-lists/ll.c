a2624821496c738fa74eadffc6b0f809767768eb
Louis Primeau <louis@Louiss-MacBook-Pro.local>
2018-11-14 00:46:43 -0500

    lab 10 working code all clear

--- lab10/linked-lists/ll.c
+++ lab10/linked-lists/ll.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ll.h"
-
 /* One of the lessons here is to see that if we want to use a function to malloc something that
  * is a POINTER in the CALLER of the function, then we must send in the ADDRESS of the POINTER
  * to that function.
- * 
+ *
  * Recap: if we want to use a function to modify a VARIABLE in the caller
  *        then the CALLER needs to send in the ADDRESS of the VARIABLE
  *
@@ -19,15 +18,16 @@
 
 int ll_add_to_head( llnode **head, int val) {
     llnode *old_head;
+
    if (head == NULL) {
       return -1;
    }
-	old_head = *head;
+	 old_head = *head;
 
    *head = ( llnode *) malloc(sizeof( llnode));
    (*head) -> val = val;
-	(*head) -> next = old_head;
-	return 0;
+	 (*head) -> next = old_head;
+	 return 0;
 }
 
 int ll_add_to_tail( llnode **head, int val) {
@@ -54,12 +54,97 @@ int ll_print( llnode *p) {
    }
 }
 
-int ll_free( llnode *p) {
+int ll_free(llnode *p) {
    if (p==NULL) {
       return -1;
    } else {
-       llnode *f=p->next;
+      llnode *f=p->next;
       free(p);
       return ll_free(f);
    }
+}
+int ll_find_by_value(llnode *pList,int val){
+  if(pList == NULL){return(-1);}
+  if((pList -> val) == val){
+    return(0);
+  }else{
+    return(ll_find_by_value(pList -> next, val));
+  }
+}
+int ll_del_from_tail(llnode **ppList){
+  llnode *holder;
+  if(ppList == NULL){return(-1);}
+  if((**ppList).next == NULL){
+    holder = (**ppList).next;
+    ll_free(holder); //Free the tail
+    *ppList = NULL; //Assign the llnode pointer we're on to null
+    return(0);
+  }else{
+    return(ll_del_from_tail(&((**ppList).next)));
+  }
+}
+int ll_del_from_head(llnode **ppList){
+  if(ppList == NULL){return(-1);}
+  free(*ppList);
+  return(0);
+}
+int ll_insert_in_order(llnode **ppList,int val){
+  llnode *newentry;
+  if(ppList == NULL){return(-1);}
+  if((**ppList).val >= val){ //if smaller than first element, stick it in.
+    newentry = (llnode *)malloc(sizeof(llnode));
+    newentry->val = val;
+    newentry->next = (**ppList).next;
+    *ppList = newentry;
+    return(0);
+  }else if((**ppList).next == NULL){//If we're at the end of the list, stick it there.
+    newentry = (llnode *)malloc(sizeof(llnode));
+    newentry->val = val;
+    newentry->next = NULL;
+    *ppList = newentry;
+    return(0);
+  }else if(val >= ((**ppList).val) && val <= ((*((**ppList).next)).val)){ //else, put it where it belongs.
+    newentry = (llnode *)malloc(sizeof(llnode));
+    newentry->val = val;
+    newentry->next = (**ppList).next;
+    (**ppList).next = newentry;
+    return(0);
+  }else{ //failing that, recurse to the next element in the ll
+    return(ll_insert_in_order(&((**ppList).next),val));
+  }
+}
+int ll_concat(llnode **pSrcA,llnode **pSrcB){
+  if(pSrcA == NULL){return(-1);}
+  if(*pSrcA == NULL){
+    *pSrcA = *pSrcB;
+    return(0);
+  }
+  else{
+    return(ll_concat(&((**pSrcA).next),pSrcB));
+  }
+}
+int ll_sort(llnode **ppList){
+  int swapped;
+  int i;
+  int hold;
+  llnode **current;
+  llnode **next;
+  while(1){
+    swapped = 0;
+    current = ppList;
+    next = &(**ppList).next;
+    while((**next).next != NULL){
+      if((**current).val > (**next).val){
+        hold = (**current).val;
+        (**current).val = (**next).val;
+        (**next).val = hold;
+        swapped = 1;
+      }
+      current = &(**current).next;
+      next = &(**next).next;
+    }
+    if(swapped == 0){
+      return(0);
+    }
+  }
 }
--- lab10/linked-lists/ll_test.c
+++ lab10/linked-lists/ll_test.c
@@ -3,6 +3,8 @@
 
 int main(void) {
    llnode *root = NULL;
+   llnode *root2 = NULL;
+   llnode *hold = NULL;
    int r=0;
    int i=0;
 
@@ -13,18 +15,31 @@ int main(void) {
    for(i=0;i<10;i=i+1){
       r=ll_add_to_tail(&root,i*100);
    }
+   r=ll_sort(&root);
+   hold = (*root).next;
+   r=ll_print(root);
+   printf("-\n");
+   r=ll_del_from_head(&root);
+   root = hold;
+   r=ll_print(root);
+   printf("-\n");
+   r=ll_insert_in_order(&root,100);
+   printf("%d\n",ll_find_by_value(root,900));
+   r=ll_del_from_tail(&root);
+   r=ll_del_from_tail(&root);
+   printf("%d\n",ll_find_by_value(root,900));
    r=ll_print(root);
    r=ll_free(root);
 
    printf("List B\n");
-	root=NULL;
-   r=ll_add_to_tail(&root,100);
-   r=ll_add_to_tail(&root,200);
-   r=ll_add_to_tail(&root,300);
+	 root2=NULL;
+   r=ll_add_to_tail(&root2,100);
+   r=ll_add_to_tail(&root2,200);
+   r=ll_add_to_tail(&root2,300);
    for(i=0;i<10;i=i+1){
-      r=ll_add_to_head(&root,i*100);
+      r=ll_add_to_head(&root2,i*100);
    }
-   r=ll_print(root);
-   r=ll_free(root);
+   r=ll_print(root2);
+   r=ll_free(root2);
    return 0;
 }
